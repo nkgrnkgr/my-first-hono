c995671434f0d843e3ce5ffe96a41116bcefe063
nkgrnkgr <beefupme@gmail.com>
2025-01-16 20:19:24 +0900

    Remove unused type definitions and third-party library imports from playground.ts

--- src/playground.ts
+++ src/playground.ts
@@ -1,37 +1,5 @@
-import { fetchDocument, type getFireStore } from "./third-party-lib.js";
-
 // https://zenn.dev/okunokentaro/articles/01gm2prv1mkqp13k3wxyg9b2bg
 export function play() {
   console.log("playground");
   return "playground";
 }
-
-// Return Type
-
-type Play = () => string;
-type Result = ReturnType<Play>;
-
-// third-party library の 関数の戻り値の型を取得すると便利
-type DB = ReturnType<typeof getFireStore>;
-
-// tupleで返ってくる値に名前をつける例
-type ReturnValue = {
-  readonly value: ReturnType<typeof fetchDocument>[0];
-  readonly isLoading: ReturnType<typeof fetchDocument>[1];
-  readonly error: ReturnType<typeof fetchDocument>[2];
-};
-
-function f(): ReturnValue {
-  const [value, isLoading, error] = fetchDocument();
-  return { value, isLoading, error };
-}
-
-const { value, isLoading, error } = f();
-
-// ReturnTypeのGenerics
-
-function getT<T>(value: T): T {
-  return value;
-}
-
-type T1 = ReturnType<<T>() => T>;
